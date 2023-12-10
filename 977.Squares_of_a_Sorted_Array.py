3b692cf6b0219b14b14c03a98b0dc18bffec282a
david hwang <88676068+david070889@users.noreply.github.com>
2023-12-10 17:24:50 +0800

    Update 977.Squares_of_a_Sorted_Array.py

--- 977.Squares_of_a_Sorted_Array.py
+++ 977.Squares_of_a_Sorted_Array.py
@@ -1,6 +1,23 @@
+#暴力法
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
         for i in range(len(nums)):
             nums[i] = pow(nums[i],2)
         nums.sort()
-        return nums 
+        return nums
+
+#O(n)的方法
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        a, b, c = 0, len(nums)-1, len(nums)-1
+        k = [float('inf')]*len(nums)
+        for i in range(b+1):
+            if nums[a]**2 > nums[b]**2:
+                k[c] = nums[a]**2
+                a += 1
+                c -= 1
+            elif nums[a]**2 <= nums[b]**2:
+                k[c] = nums[b]**2
+                b -= 1
+                c -= 1
+        return k
