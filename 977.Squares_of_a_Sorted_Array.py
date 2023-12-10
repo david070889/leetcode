c29f403b259bf143baf958b9c9c45e298c72e9f4
david hwang <88676068+david070889@users.noreply.github.com>
2023-12-10 17:30:33 +0800

    Update 977.Squares_of_a_Sorted_Array.py

--- 977.Squares_of_a_Sorted_Array.py
+++ 977.Squares_of_a_Sorted_Array.py
@@ -6,7 +6,7 @@ class Solution:
         nums.sort()
         return nums
 
-#O(n)的方法
+#double index, O(n)的方法(共只經過n個index的array一次)
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
         a, b, c = 0, len(nums)-1, len(nums)-1
@@ -21,3 +21,18 @@ class Solution:
                 b -= 1
                 c -= 1
         return k
+
+#範例答案(更好的運用所有變數)
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        l, r, i = 0, len(nums)-1, len(nums)-1
+        res = [float('inf')] * len(nums) # 需要提前定义列表，存放结果
+        while l <= r:
+            if nums[l] ** 2 < nums[r] ** 2: # 左右边界进行对比，找出最大值
+                res[i] = nums[r] ** 2
+                r -= 1 # 右指针往左移动
+            else:
+                res[i] = nums[l] ** 2
+                l += 1 # 左指针往右移动
+            i -= 1 # 存放结果的指针需要往前平移一位
+        return res
