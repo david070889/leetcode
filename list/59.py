81f81d7271346c17d0e22cfb2dea4c87bd057c09
david hwang <88676068+david070889@users.noreply.github.com>
2023-12-11 22:16:52 +0800

    Update 59.py

--- list/59.py
+++ list/59.py
@@ -1,33 +1,56 @@
 class Solution:
     def generateMatrix(self, n: int) -> List[List[int]]:
-        s = n**2
-        i = 0
-        j = 0
-        h = 1
-        k =[]
+        matrix = []
+        insert = []
         for i in range(n):
-            k.append([0]*n)
-        while h <= s:
-            while j < n:
-                k[i][j] = h
-                j += 1
-                h += 1
-            j -= 1 
-            while j > i:
-                k[i][j] = h
+            matrix.append([0]*n)
+        i = 0
+        j = n-1
+        L = 0
+        a = 0 #starting point
+        for x in  range(n**2):
+            insert.append(x+1)
+
+        if n % 2 == 1:
+            layer = int((n+1)/2)
+            while i < layer - 1:    
+                for q in range(L,j):
+                    matrix[L][q] = insert[q-L]
+                a += j-2*L
+                for q in range(L,j):
+                    matrix[q][j-L] = insert[a+q-L]
+                a += j-2*L
+                for q in range(j-L-1,L-1,-1):
+                    matrix[j-L][q+1] = insert[a+j-1-q-L]
+                a += j-2*L
+                for q in range(j-L-1,L-1,-1):
+                    matrix[q+1][L] = insert[a+j-1-q-L]
+                a += j-2*L
                 i += 1
-                h += 1
-            i -= 1
-            while j == i:
-                k[i][j] = h
                 j -= 1
-                h += 1
-            j += 1
-            while i > j:
-                k[i][j] = h
-                i -= 1
-                h += 1
-            i += 1
-        return k
+                L += 1
+            if i == layer-1:
+                matrix[layer-1][layer-1] = n**2
+            
+
+        else:
+            layer = n / 2
+            while i < layer:  
+                for q in range(L,j):
+                    matrix[L][q] = insert[q-L]
+                a += j-2*L
+                for q in range(L,j):
+                    matrix[q][j-L] = insert[a+q-L]
+                a += j-2*L
+                for q in range(j-L-1,L-1,-1):
+                    matrix[j-L][q+1] = insert[a+j-1-q-L]
+                a += j-2*L
+                for q in range(j-L-1,L-1,-1):
+                    matrix[q+1][L] = insert[a+j-1-q-L]
+                a += j-2*L
+                i += 1
+                j -= 2
+                L += 1
+        return matrix
         
 
