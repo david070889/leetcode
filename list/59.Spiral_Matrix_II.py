2dbf22de6ed08be0b9b8a04631bd213f58946a26
david hwang <88676068+david070889@users.noreply.github.com>
2023-12-13 15:44:18 +0800

    Update 59.Spiral_Matrix_II.py

--- list/59.Spiral_Matrix_II.py
+++ list/59.Spiral_Matrix_II.py
@@ -5,17 +5,17 @@ class Solution:
         for i in range(n): #初始化matrix
             matrix.append([0]*n)
         i = 0 #控制while迴圈 iteration的次數
-        j = n-1 #最外圍每次插入的資料個數
+        j = n-1 #控制每次插入的個數
         L = 0 #現在處於得layer數 外->內為 0->layer-1 
         a = 0 #starting point
-        for x in  range(n**2):
+        for x in  range(n**2): #將準備插入的1到n^2都在list中準備好
             insert.append(x+1)
 
-        if n % 2 == 1:
-            layer = int((n+1)/2)
-            while i < layer - 1:    
-                for q in range(L,j):
-                    matrix[L][q] = insert[a+q-L]
+        if n % 2 == 1: #n為odd
+            layer = int((n+1)/2) #算出總共有幾層(中間的單個值也算一層)
+            while i < layer - 1: #我們控制這裡的遞迴 只會到從外圈開始屬的倒數第二層 #雖然一開始一次插入j個，但是用range取範圍index只會到j-1，剛好符合
+                for q in range(L,j): #從最層的第一個開始 m(0,0) 到 m(0,j-1)
+                    matrix[L][q] = insert[a+q-L] #insert[0:j-1]
                 a += n-1-2*L
                 for q in range(L,j):
                     matrix[q][j] = insert[a+q-L]
@@ -32,8 +32,8 @@ class Solution:
             matrix[layer-1][layer-1] = n**2
             
 
-        else :
-            layer = int(n / 2)
+        else : #n為even
+            layer = int(n / 2) #算出總共有幾層
             while i < layer - 1:  
                 for q in range(L,j):
                     matrix[L][q] = insert[a+q-L]
